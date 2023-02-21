e9c00127b0f26e9b71ea1e1a16596021232653ad
Yoswaris Lawpaiboon <22832362+kiznick@users.noreply.github.com>
2023-02-21 18:28:42 +0700

    s

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -11,7 +11,7 @@ export default function Error404() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={`flex h-screen`}>
-        <div className={`m-auto`}>
+        <div className={`m-auto p-8`}>
           <h1 className={`text-9xl text-red-500`}>404</h1>
           <h2 className={`text-6xl mb-3`}>Page not found.</h2>
           <p className={`text-xl`}>The page you are looking for does not exist.</p>
