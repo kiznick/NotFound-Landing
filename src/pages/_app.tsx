528d4ce46ccbe51c87969e5505ffd21042538fdd
Yoswaris Lawpaiboon <22832362+kiznick@users.noreply.github.com>
2023-02-21 16:52:13 +0700

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { Noto_Sans_Thai } from '@next/font/google'
+
+const noto = Noto_Sans_Thai({
+  subsets: ['thai'],
+  weight: ['400', '700']
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`${noto.className}`}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
