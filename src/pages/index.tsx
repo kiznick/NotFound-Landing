4961ac88b97f8861ba026faeb4d5b932d10abbbe
Yoswaris Lawpaiboon <22832362+kiznick@users.noreply.github.com>
2023-03-15 00:47:27 +0700

    typo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Error404(props: AppProps) {
       </Head>
       <div className={`flex h-screen`}>
         <div className={`m-auto p-8`}>
-          <h1 className={`text-9xl text-red-500`}>4d04</h1>
+          <h1 className={`text-9xl text-red-500`}>404</h1>
           <h2 className={`text-6xl mb-3`}>Page not found.</h2>
           <p className={`text-xl`}>The page you are looking for does not exist.</p>
           <p className={`text-xl`}>Please check the URL in the address bar and try again.</p>
